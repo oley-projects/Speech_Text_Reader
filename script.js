a7e1653787d167293925e6c6a2766ff09de8c6c1
oley-projects <oley.for.git@gmail.com>
2021-06-12 23:18:06 +0300

    speech boxes styling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const main = document.querySelector('main'),
       closeBtn = document.getElementById('close');
 
 const data = [
-    { image: './img/dring.jpg', text: "I'm Thirsty" },
+    { image: './img/drink.jpg', text: "I'm Thirsty" },
     { image: './img/food.jpg', text: "I'm Hungry" },
     { image: './img/tired.jpg', text: "I'm Tired" },
     { image: './img/hurt.jpg', text: "I'm Hurt" },
